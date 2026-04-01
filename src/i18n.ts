4efe64bdb9709737a4712d9d9352b547feb2ff62
bekulugbekov <bekulugbekov01@gmail.com>
2026-04-01 09:35:52 +0500

    Suppress console logs for i18next and Locize
    Override console.log to suppress specific log messages.

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,13 @@ import uz from './locales/uz.json';
 import ru from './locales/ru.json';
 import en from './locales/en.json';
 
+const originalLog = console.log;
+console.log = (...args) => {
+  if (typeof args[0] === 'string' && (args[0].includes('i18next') || args[0].includes('Locize'))) {
+    return; // Agar yozuvda i18next yoki Locize bo'lsa, uni chiqarma
+  }
+  originalLog(...args);
+};
 i18n
   .use(initReactI18next)
   .init({
